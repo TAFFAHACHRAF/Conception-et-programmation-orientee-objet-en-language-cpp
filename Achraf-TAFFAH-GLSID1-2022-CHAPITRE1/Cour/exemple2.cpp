e0516c35d8fe370ecbc3db35e87faac5a33f676f
Achraf Taffah <99338171+TAFFAHACHRAF@users.noreply.github.com>
2022-04-29 17:36:16 +0200

    Delete Achraf-TAFFAH-GLSID1-2022-CHAPITRE1 directory

--- Achraf-TAFFAH-GLSID1-2022-CHAPITRE1/Cour/exemple2.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-	float Pi = 3.14;
-	cout << " La valeur de Pi est : "; //Cas 1
-	cout << Pi;
-	
-	cout << "\n La valeur de Pi est : " << Pi; //Cas 2
-}
--- Achraf-TAFFAH-GLSID1-2022-CHAPITRE1/Cour/exemple3.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-	int N;
-	cout <<" Entrer un nombre entier : ";
-	cin >> N;
-	cout << "Le carré du nombre entié est :" << N*N;
-	return 0;
-}
